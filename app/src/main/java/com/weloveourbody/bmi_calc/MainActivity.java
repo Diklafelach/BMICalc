e99d475290403579e2f1752a0d6a825871f5b485
Dikla Felach <diklafelach1@gmail.com>
2021-04-05 19:56:43 +0300

    Finish the Gui

--- app/src/main/java/com/weloveourbody/bmi_calc/MainActivity.java
+++ app/src/main/java/com/weloveourbody/bmi_calc/MainActivity.java
@@ -9,6 +9,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //set the content of the screen.
+        //R.layout.* references any layout resource that have been create
         setContentView(R.layout.activity_main);
     }
 }
