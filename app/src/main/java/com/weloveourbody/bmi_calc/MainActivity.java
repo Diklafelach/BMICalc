666374924e710e904a5f6fe0bce0fbec10dbd05d
Dikla Felach <diklafelach1@gmail.com>
2021-04-10 20:41:13 +0300

    linking between xml and java

--- app/src/main/java/com/weloveourbody/bmi_calc/MainActivity.java
+++ app/src/main/java/com/weloveourbody/bmi_calc/MainActivity.java
@@ -3,6 +3,11 @@ package com.weloveourbody.bmi_calc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +17,15 @@ public class MainActivity extends AppCompatActivity {
         //set the content of the screen.
         //R.layout.* references any layout resource that have been create
         setContentView(R.layout.activity_main);
+
+
+        //Connection between XML and JAVA
+        TextView result = findViewById(R.id.text_view_result);
+        Button calculate = findViewById(R.id.button_calculate);
+        EditText ageEditText = findViewById(R.id.edit_text_age);
+        EditText weightEditText = findViewById(R.id.edit_text_weight);
+        EditText heightEditText = findViewById(R.id.edit_text_height);
+        RadioButton femaleButton = findViewById(R.id.radio_button_female);
+        RadioButton maleButton = findViewById(R.id.radio_button_male);
     }
 }
